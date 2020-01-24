17715500ad9ff1720f6e50e39029ca1791142408
David Cha <davidcha17@yahoo.com>
2020-01-24 11:58:43 -0500

    complete

--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -6,4 +6,22 @@ class VendorSweet < ApplicationRecord
     validates :price, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0} 
 
+    validates_uniqueness_of :vendor_id, :scope => [:sweet_id]
+    # validates_uniqueness_of :sweet_id, :scoope => [:vendor_id]
+
+
+
+    private 
+
+
+#There should only be one `VendorSweet` for any pair of `Vendor`
+#  and `Sweet`.
+
+# if the pair exist, do not make the pair 
+
+# Add a validation to ensure that there is only one VendorSweet 
+# for a Vendor and Sweet pair. Update your
+#  error handling so that this new validation is also displayed
+#   to the user.
+
 end

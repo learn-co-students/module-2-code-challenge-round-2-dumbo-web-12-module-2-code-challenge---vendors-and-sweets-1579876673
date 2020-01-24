fbe6d89a83d5222e02a503c8facfa83fbb68c063
David Cha <davidcha17@yahoo.com>
2020-01-24 11:00:24 -0500

    DONE PRE VAL

---
+++ app/controllers/vendor_sweets_controller.rb
@@ -0,0 +1,33 @@
+class VendorSweetsController < ApplicationController
+
+    def new
+        @vendor_sweet = VendorSweet.new 
+        @vendors = Vendor.all
+        @sweets = Sweet.all
+    end
+
+    def create
+
+        @vendor_sweet = VendorSweet.create(vendor_sweet_params) 
+
+        if @vendor_sweet.valid?
+        redirect_to vendor_path(@vendor_sweet.vendor)
+        else 
+            flash[:errors] = @vendor_sweet.errors.full_messages
+            redirect_to new_vendor_sweet_path
+        end 
+
+    end 
+
+
+
+    private 
+
+    def vendor_sweet_params
+        params.require(:vendor_sweet).permit(:vendor_id, :sweet_id, :price)
+    end
+    # def person_params
+    #     params.require(:person).permit(:name, :age)
+    #   end
+
+end
